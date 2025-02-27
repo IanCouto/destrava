e8f529831e6a2473ea33277c9b4614736ec65043
iandepaula <ian.depaula@logicinfo.com>
2025-02-27 14:15:17 -0300

    colorPick2

--- src/app/colorPick2/page.tsx
+++ src/app/colorPick2/page.tsx
@@ -2,16 +2,16 @@
 
 import { useState } from "react";
 import { Button } from "@/ui/button";
-import { Card } from "@/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/ui/card";
 import { Input } from "@/ui/input";
 import { Badge } from "@/ui/badge";
 import "@/styles/globals.css";
+import { Alert } from "@/components/ui/alert";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
 
 const colors = [
-  "--background", "--foreground", "--card", "--card-foreground", "--popover", "--popover-foreground",
-  "--primary", "--primary-foreground", "--secondary", "--secondary-foreground", "--muted", "--muted-foreground",
-  "--accent", "--accent-foreground", "--destructive", "--destructive-foreground", "--border", "--input", "--ring",
-  "--chart-1", "--chart-2", "--chart-3", "--chart-4", "--chart-5"
+  "--background"
 ];
 
 export default function ColorPick() {
@@ -23,20 +23,49 @@ export default function ColorPick() {
   };
 
   return (
-    <section className={`grid grid-cols-8 gap-4 w-full h-screen p-4 ${isDarkMode ? "dark" : ""}`}>
-      {colors.map(color => (
-        <Card key={color} className="col-span-1 p-4" style={{ backgroundColor: `hsl(var(${color}))` }}>
-          <Button onClick={toggleTheme} className="mt-2">
-            Button
-          </Button>
-          <Input className="mt-2" placeholder="Input" />
-          <Badge className="mt-2">
-            Badge
-          </Badge>
-          <h2 className="text-lg" style={{ color: 'black' }}>{color}</h2>
-          <h2 className="text-lg" style={{ color: 'white' }}>{color}</h2>
-        </Card>
-      ))}
+    <section className={`flex justify-center md:items-center md:pt-0 pt-8 h-screen ${isDarkMode ? "dark" : ""}`}>
+      <div className="px-4">
+        {colors.map(color => (
+          <Card key={color}>
+            <CardHeader>
+              <CardTitle>Color Picker</CardTitle>
+            </CardHeader>
+            <CardDescription className="pl-6 pb-2">Description</CardDescription>
+            <CardContent className="grid grid-cols-3 gap-4">
+              <Button variant={"default"} onClick={toggleTheme}>default</Button>
+              <Button variant={"secondary"} onClick={toggleTheme}>secondary</Button>
+              <Button variant={"destructive"} onClick={toggleTheme}>destructive</Button>
+              <Button variant={"ghost"} onClick={toggleTheme}>ghost</Button>
+              <Button variant={"link"} onClick={toggleTheme}>link</Button>
+              <Button variant={"outline"} onClick={toggleTheme}>outline</Button>
+            </CardContent>
+            <CardContent className="grid grid-cols-4 gap-4">
+              <Button size={"lg"} onClick={toggleTheme}>lg</Button>
+              <Button size={"default"} onClick={toggleTheme}>default</Button>
+              <Button size={"sm"} onClick={toggleTheme}>sm</Button>
+              <Button size={"icon"} onClick={toggleTheme}>icon</Button>
+            </CardContent>
+            <CardContent>
+              <Input placeholder="Input" />
+            </CardContent>
+            <CardContent className="grid grid-cols-2 gap-4">
+              <Badge variant={"default"}>default</Badge>
+              <Badge variant={"secondary"}>secondary</Badge>
+              <Badge variant={"destructive"}>destructive</Badge>
+              <Badge variant={"outline"}>outline</Badge>
+            </CardContent>
+            <CardContent className="flex flex-col gap-2">
+              <Alert variant={"default"}>default</Alert>
+              <Alert variant={"destructive"}>destructive</Alert>
+            </CardContent>
+            <CardContent className="flex items-center gap-2">
+              <Switch onClick={toggleTheme}/>
+              <Label> Toggle Theme</Label>
+            </CardContent>
+            <CardFooter>Footer</CardFooter>
+          </Card>
+        ))}
+      </div>
     </section>
   );
 }
