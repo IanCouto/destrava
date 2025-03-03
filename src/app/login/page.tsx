713796b0750a31570c1d9711ef09069269daf25e
Ian Couto <48738352+IanCouto@users.noreply.github.com>
2025-03-03 10:09:54 -0300

    lint

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,33 +1,33 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from 'react'
-import { createClient } from '@/utils/supabase/client'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useState, useEffect } from 'react';
+import { createClient } from '@/utils/supabase/client';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { Session } from '@supabase/supabase-js';
 
-const supabase = createClient()
+const supabase = createClient();
 
 export default function App() {
-  const [session, setSession] = useState(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
-    return () => subscription.unsubscribe()
-  }, [])
+    return () => subscription.unsubscribe();
+  }, []);
 
   if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-  }
-  else {
-    return (<div>Logged in!</div>)
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />);
+  } else {
+    return (<div>Logged in!</div>);
   }
 }
